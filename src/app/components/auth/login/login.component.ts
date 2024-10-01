1dc3f1a0a80c0c10061602d4ec4f8ea286877c2e
Harr-izle <harrietbuadee@gmail.com>
2024-10-01 08:21:49 +0000

    implements logic to get the login form to work with the authservice

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form!: FormGroup;
+  message: string = '';
+  isError: boolean = false;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]]
+    });
+  }
+
+  login(): void {
+    if (this.form.valid) {
+      this.authService.login(this.form.value.email, this.form.value.password)
+        .subscribe({
+          next: (result) => {
+            this.message = result.message;
+            this.isError = !result.success;
+            if (result.success) {
+              this.form.reset();
+            }
+          },
+          error: (error) => {
+            this.message = error.message;
+            this.isError = true;
+          }
+        });
+    }
+  }
+
 
 }
