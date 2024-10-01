d231b522b8105c07e37a55534d3294885ebd10a5
Harr-izle <harrietbuadee@gmail.com>
2024-10-01 06:47:08 +0000

    creates environment for the application

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: 'fireblog-2da5b',
+        appId: '1:61847321071:web:9d6dc58fc83cd8fac95e1a',
+        storageBucket: 'fireblog-2da5b.appspot.com',
+        apiKey: 'AIzaSyDUddjXL1DRPsFJq-UlewRzvynaxaxofTo',
+        authDomain: 'fireblog-2da5b.firebaseapp.com',
+        messagingSenderId: '61847321071',
+        measurementId: 'G-HZ1LLMBDZF',
+    },
+};
