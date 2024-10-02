069c5b7ef9820c5e9df79da94b32d35787d05cb3
Harr-izle <harrietbuadee@gmail.com>
2024-10-02 15:07:39 +0000

    updates post ts

--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,8 +1,9 @@
-export interface IPost{
+export interface IPost {
     id?: string;
     title: string;
     content: string;
     authorId: string;
+    authorName: string;
     createdAt: Date;
     updatedAt: Date;
-}
+  }
