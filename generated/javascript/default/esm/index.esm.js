6038b7d0edca71a40368dc3170e1dd3164ea14d1
Harr-izle <harrietbuadee@gmail.com>
2024-10-03 10:01:39 +0000

    deploys application

---
+++ generated/javascript/default/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'firebase-blog',
+  location: 'us-central1'
+};
+
