c8fb74c91b8bcb86bd38bd4fb609844d29a0d8ac
Harr-izle <harrietbuadee@gmail.com>
2024-10-04 16:40:13 +0000

    fixes UI blinking bug during post update

--- src/app/components/blog/post/post.component.ts
+++ src/app/components/blog/post/post.component.ts
@@ -1,28 +1,32 @@
-
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, switchMap, tap } from 'rxjs';
 import { IPost } from '../../../models/post';
 import { AuthService } from '../../../services/auth/auth.service';
 import { BlogpostService } from '../../../services/blogpost/blogpost.service';
 import { CommentComponent } from '../comment/comment.component';
 
-
-
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReactiveFormsModule,CommentComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, CommentComponent],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+  styleUrl: './post.component.css',
 })
 export class PostComponent {
-  post$: Observable<IPost | undefined>;
+  private postSubject = new BehaviorSubject<IPost | undefined>(undefined);
+  post$ = this.postSubject.asObservable();
   currentUserId: string | null = null;
   isEditing = false;
   editForm: FormGroup;
+  private subscriptions: Subscription = new Subscription();
 
   constructor(
     private route: ActivatedRoute,
@@ -35,71 +39,84 @@ export class PostComponent {
       title: ['', Validators.required],
       content: ['', Validators.required]
     });
-    this.post$ = new Observable<IPost | undefined>();
   }
 
   ngOnInit() {
-    this.post$ = this.route.paramMap.pipe(
-      switchMap(params => {
-        const id = params.get('id');
-        return id ? this.blogService.getPost(id) : new Observable<undefined>();
-      })
+    this.subscriptions.add(
+      this.route.paramMap.pipe(
+        switchMap(params => {
+          const id = params.get('id');
+          return id ? this.blogService.getPost(id) : new Observable<undefined>();
+        }),
+        tap(post => this.postSubject.next(post))
+      ).subscribe()
     );
 
-    this.authService.getCurrentUser().subscribe(user => {
-      this.currentUserId = user ? user.uid : null;
-    });
+    this.subscriptions.add(
+      this.authService.getCurrentUser().subscribe(user => {
+        this.currentUserId = user ? user.uid : null;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   startEdit() {
     this.isEditing = true;
-    this.post$.subscribe(post => {
-      if (post) {
-        this.editForm.patchValue({
-          title: post.title,
-          content: post.content
-        });
-      }
-    });
+    const post = this.postSubject.getValue();
+    if (post) {
+      this.editForm.patchValue({
+        title: post.title,
+        content: post.content
+      });
+    }
   }
 
   cancelEdit() {
     this.isEditing = false;
   }
 
-  updatePost() {
+  updatePost(event?: Event) {
+    if (event) {
+      event.preventDefault(); // Prevent form submission if event is provided
+    }
     if (this.editForm.valid) {
-      this.post$.subscribe(post => {
-        if (post && post.id) {
-          this.blogService.updatePost(post.id, this.editForm.value)
-            .subscribe({
-              next: () => {
-                this.isEditing = false;
-                // Refresh the post data
-                this.post$ = this.blogService.getPost(post.id!);
-              },
-              error: (error: any) => console.error('Error updating post:', error)
-            });
-        }
-      });
+      const post = this.postSubject.getValue();
+      if (post && post.id) {
+        const updatedPost: IPost = {
+          ...post,
+          ...this.editForm.value
+        };
+        this.blogService.updatePost(post.id, this.editForm.value)
+          .subscribe({
+            next: () => {
+              this.isEditing = false;
+              this.postSubject.next(updatedPost);
+            },
+            error: (error: any) => {
+              console.error('Error updating post:', error);
+              // Optionally, you can show an error message to the user here
+            }
+          });
+      }
     }
   }
 
   deletePost() {
-    this.post$.subscribe(post => {
-      if (post && post.id) {
-        this.blogService.deletePost(post.id)
-          .subscribe({
-            next: () => {
-              this.router.navigate(['/']);
-            },
-            error: (error: any) => console.error('Error deleting post:', error)
-          });
-      }
-    });
+    const post = this.postSubject.getValue();
+    if (post && post.id) {
+      this.blogService.deletePost(post.id)
+        .subscribe({
+          next: () => {
+            this.router.navigate(['/']);
+          },
+          error: (error: any) => console.error('Error deleting post:', error)
+        });
+    }
   }
 
-  
   navigateToProfile() {
     this.router.navigate(['/profile']);
   }
